e90dcb46b95595ceea3f56dddf49111b9472f3c7
mrandypoet <dthuangjiaxing@gmail.com>
2022-09-02 01:58:33 +0100

    terminal update

--- nvim/lua/config/treesitter.lua
+++ nvim/lua/config/treesitter.lua
@@ -3,7 +3,7 @@ require("nvim-treesitter.configs").setup {
   sync_install = false,
   highlight = {
     enable = true,
-    disable = { "c", "cpp", "java" }
+    disable = { "java" }
   },
   playground = {
     enable = true,
