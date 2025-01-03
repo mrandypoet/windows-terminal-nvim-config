e1fb72ce49291e5e714e5b6549650284a1082d68
Jiaxing Huang <jiaxing.huang@v-nova.com>
2025-01-03 10:48:43 +0000

    all changes

--- nvim/lua/config/treesitter.lua
+++ nvim/lua/config/treesitter.lua
@@ -1,5 +1,11 @@
+vim.filetype.add({
+	extension = {
+		comp = "glsl", -- Treat .comp files as GLSL
+	},
+})
+
 require("nvim-treesitter.configs").setup {
-	ensure_installed = { "c", "cpp", "lua", "cmake", "latex", "json", "python", "markdown" },
+	ensure_installed = { "c", "cpp", "lua", "cmake", "latex", "json", "python", "markdown", "glsl" },
 	sync_install = false,
 	highlight = {
 		enable = true,
@@ -8,6 +14,7 @@ require("nvim-treesitter.configs").setup {
 	playground = {
 		enable = true,
 	},
+	fold = { enable = true },
 }
 
 require 'nvim-treesitter.install'.compilers = { "clang", "gcc" }
